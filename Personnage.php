fd40a65beb612386c63a143f3ff8c87ebbb09792
Philippe PERECHODOV <philippe.perechodov@free.fr>
2021-01-09 15:25:10 +0100

    Utiliser la classe (1)

--- Personnage.php
+++ Personnage.php
@@ -1,28 +1,77 @@
 <?php
 class Personnage
 {
-  private $_force;        // La force du personnage
+  private $_force; // Sa force
   private $_localisation; // Sa localisation
-  private $_experience;   // Son expérience
-  private $_degats;       // Ses dégâts
+  private $_experience = 0; // Son expérience
+  private $_degats = 0; // Ses dégâts
+
+  // Getters
+  public function getForce()
+  {
+    return $this->_force;
+  }
+
+  public function getLocalisation()
+  {
+    return $this->_localisation;
+  }
+
+  public function getExperience()
+  {
+    return $this->_experience;
+  }
+
+  public function getDegats()
+  {
+    return $this->_degats;
+  }
+
+    // Setters
+    public function setForce($force)
+    {
+      $this->_force = $force;
+    }
   
-  public function deplacer() // Une méthode qui déplacera le personnage (modifiera sa localisation).
-  {
+    public function setLocalisation($localisation)
+    {
+      $this->_localisation = $localisation;
+    }
+  
+    public function setExperience($experience)
+    {
+      $this->_experience = $experience;
+    }
+  
+    public function setDegats($degats)
+    {
+      $this->_degats = $degats;
+    }
 
+  // Méthodes
+    public function ATH($numéro) // Pour afficher les différents éléments du personnage
+  {
+    echo 'Personnage n°' . $numéro . ' :<br>Expérience = ' . $this->_experience . '<br> Force = ' . $this->_force . '<br> Dégats = ' . $this->_degats . '<br> Localisation = ' . $this->_localisation . '<br>';
   }
   
-  public function frapper() // Une méthode qui frappera un personnage (suivant la force qu'il a).
+  public function deplacer() // Pour déplacer le personnage (modifier sa localisation).
   {
-
+    
   }
   
-  public function gagnerExperience() // Une méthode augmentant l'attribut $experience du personnage.
+  public function frapper(Personnage $persoAFrapper) // Pour frapper un personnage (suivant la force qu'il a).
   {
-
+    $persoAFrapper->_degats += $this->_force;
+  }
+  
+  public function gagnerExperience() // Pour augmenter l'attribut $experience du personnage.
+  {
+    $this->_experience = $this->_experience + 1;
   }
 
-  public function parler() // Une méthode pour faire parler le personnage (afficher un texte).
+  public function parler($numéro) // Pour faire parler le personnage (afficher un texte).
   {
-    echo 'Je suis un personnage !';
+    echo 'Bonjour, je suis le personnage n°' . $numéro . ' !<br>';
   }
+
 }
--- index.php
+++ index.php
@@ -12,8 +12,26 @@
     <?php
         require 'Personnage.php';
 
-        $perso = new Personnage;
-        $perso->parler();
+        $perso1 = new Personnage;
+        $perso1->setForce(random_int(10, 20));
+        $perso2 = new Personnage;
+        $perso2->setForce(random_int(10, 20));
+        echo 'Fiche de base :<br>';
+        $perso1->parler("1");
+        $perso1->ATH("1");
+        $perso2->parler("2");
+        $perso2->ATH("2");
+        echo 'Combat, tour n°1 :<br>';
+        $perso1->frapper($perso2);
+        $perso1->gagnerExperience();
+        $perso1->ATH("1");
+        $perso2->ATH("2");
+        echo 'Combat, tour n°2 :<br>';
+        $perso2->frapper($perso1);
+        $perso2->gagnerExperience();
+        $perso1->ATH("1");
+        $perso2->ATH("2");
+        
 
     ?>
 
